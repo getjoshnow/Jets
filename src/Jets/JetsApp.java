1b1db7d6bac1eb060ddcf536306d18eca6e3a730
Josh <robbins@Joshs-MacBook-Pro.local>
2019-07-26 14:42:13 -0600

    Initial Setup Comit

---
+++ src/Jets/JetsApp.java
@@ -0,0 +1,11 @@
+package Jets;
+
+public class JetsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Jets.Launch();
+	}
+
+}
---
+++ src/Jets/StringSplitPlay.java
@@ -0,0 +1,5 @@
+package Jets;
+
+public class StringSplitPlay {
+
+}
