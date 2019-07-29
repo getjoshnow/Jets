d96d0cfebb25ffe559fcb2aa3548a408528e88e5
Josh <robbins@Joshs-MacBook-Pro.local>
2019-07-29 08:44:17 -0600

    Added User Story #9 Stretch Goal

--- src/Jets/JetImpl.java
+++ src/Jets/JetImpl.java
@@ -29,9 +29,14 @@ public class JetImpl extends Jet {  //User Story #9
 		int userPlaneRange = kb.nextInt();
 		System.out.print("\nEnter price of the plane:" );
 		long userPlanePrice = kb.nextLong();
+		System.out.print("is this a (1) fighter, or  (2) cargo plane:" );
+		int userPlaneType = kb.nextInt();
+		if (userPlaneType == 1) {
+		jets.add(new FighterJet(userPlaneName, userPlaneSpeed, userPlaneRange, userPlanePrice));
+		}
+		else {
+		jets.add(new CargoPlane(userPlaneName, userPlaneSpeed, userPlaneRange, userPlanePrice));
+		}
 		
-		
-		jets.add(new JetImpl(userPlaneName, userPlaneSpeed, userPlaneRange, userPlanePrice));
-
 	}
 }
